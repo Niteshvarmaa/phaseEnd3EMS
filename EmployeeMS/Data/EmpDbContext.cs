6dd494955f43f992ce1087d1da7e24720c25db5f
Niteshvarmaa <niteshvarma126@gmail.com>
2024-02-09 20:25:54 +0530

    Add project files.

---
+++ EmployeeMS/Data/EmpDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EmployeeMS.Models;
+
+namespace EmployeeMS.Data
+{
+    public class EmpDbContext : DbContext
+    {
+        public EmpDbContext (DbContextOptions<EmpDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EmployeeMS.Models.EmpProfile> EmpProfile { get; set; } = default!;
+    }
+}
